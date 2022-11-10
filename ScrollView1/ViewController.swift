1a3409f27ca243b03cf1e48d225050a6cda37b4b
TalibIOS <goldsikkait13@gmail.com>
2022-11-10 12:58:14 +0530

    Initial Commit

---
+++ ScrollView1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ScrollView1
+//
+//  Created by Talib on 16/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
